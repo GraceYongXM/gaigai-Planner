32ec516227a1d5bbac4ef42d6fdd26cb22ee2ac7
Teresa <teresasee02@gmail.com>
2022-07-06 14:38:06 +0800

    users can join by invitation code, and owner can accept/reject

--- lib/services/event_service.dart
+++ lib/services/event_service.dart
@@ -1,12 +1,136 @@
 import 'dart:developer';
 import 'package:supabase/supabase.dart';
-import 'package:uuid/uuid.dart';
 
 import '../models/event_details.dart';
+import '../models/self_invitation.dart';
+import '../models/user.dart' as model;
 
 class EventService {
   final _client = SupabaseClient('https://xvjretabvavhxqyaftsr.supabase.co',
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2anJldGFidmF2aHhxeWFmdHNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ1OTEzMzYsImV4cCI6MTk3MDE2NzMzNn0.3Iuz9BYCPWVDbELfJa2b_jzU9OVzW95St099K2RS_YU');
+
+  void updateRequest(
+      {required String eventID,
+      required String userID,
+      required bool accepted,
+      required String invitationID}) async {
+    String status = 'rejected';
+    if (accepted) {
+      status = 'accepted';
+      final insert1 = await _client.from('events_users').insert({
+        'event_id': eventID,
+        'user_id': userID,
+      }).execute();
+      log(insert1.error == null
+          ? 'insert1 success'
+          : 'Error in insert1: ${insert1.error!.message}');
+    }
+    final response = await _client
+        .from('self_invitations')
+        .update({'status': status})
+        .eq('id', invitationID)
+        .execute();
+    log(response.error == null
+        ? 'updateRequest success'
+        : 'Error in updateRequest: ${response.error!.message}');
+  }
+
+  Future<List<SelfInvitation>> getSelfInvitations(String eventID) async {
+    final response = await _client
+        .from('self_invitations')
+        .select()
+        .match({'event_id': eventID, 'status': 'pending'}).execute();
+    if (response.error == null) {
+      final results = response.data as List<dynamic>;
+      List<SelfInvitation> selfInvitations = results
+          .map((e) => SelfInvitation(e['id'], e['user_id'], eventID, 'pending',
+              DateTime.parse(e['created_at'])))
+          .toList();
+      if (selfInvitations.isNotEmpty) {
+        return selfInvitations;
+      }
+      log('getSelfInvitations success');
+    } else {
+      log('Error in getSelfInvitations: ${response.error!.message}');
+    }
+    return [];
+  }
+
+  Future<List<model.User>> getInvitationInfo(String eventID) async {
+    List<String> userIDs =
+        (await getSelfInvitations(eventID)).map((e) => e.userID).toList();
+    List<model.User> users = [];
+    for (String id in userIDs) {
+      final response =
+          await _client.from('users').select().eq('id', id).execute();
+      final results = (response.data as List<dynamic>)[0];
+      users.add(model.User(
+        id,
+        results['username'],
+        results['mobileNo'],
+        results['email'],
+        results['display_name'],
+        results['bio'],
+        DateTime.parse(results['createTime']),
+      ));
+    }
+    return users;
+  }
+
+  Future<bool> invitationCodeExists(String eventID) async {
+    final response = await _client
+        .from('events_details')
+        .select()
+        .eq('event_id', eventID)
+        .execute();
+    if (response.error == null) {
+      final results = response.data as List<dynamic>;
+      return results.isNotEmpty;
+    } else {
+      log('Error invitationCodeExists: ${response.error!.message}');
+      return false;
+    }
+  }
+
+  Future<bool> requestExists(String eventID, String userID) async {
+    final response = await _client
+        .from('self_invitations')
+        .select()
+        .match({'event_id': eventID, 'user_id': userID}).execute();
+    if (response.error == null) {
+      final results = response.data as List<dynamic>;
+      return results.isNotEmpty;
+    } else {
+      log('Error invitationCodeExists: ${response.error!.message}');
+      return false;
+    }
+  }
+
+  Future<bool> userAlreadyInEvent(String eventID, String userID) async {
+    final response = await _client
+        .from('events_users')
+        .select()
+        .match({'event_id': eventID, 'user_id': userID}).execute();
+    if (response.error == null) {
+      final results = response.data as List<dynamic>;
+      return results.isNotEmpty;
+    } else {
+      log('Error userAlreadyInEvent: ${response.error!.message}');
+      return false;
+    }
+  }
+
+  void insertSelfInvitation(String eventID, String userID) async {
+    final response = await _client.from('self_invitations').insert({
+      'user_id': userID,
+      'event_id': eventID,
+    }).execute();
+    if (response.error == null) {
+      log('insertSelfInvitation success');
+    } else {
+      log('insertSelfInvitation error: ${response.error!.message}');
+    }
+  }
 
   Future<bool> uniqueEventName(String name, String userID) async {
     final response = await _client
