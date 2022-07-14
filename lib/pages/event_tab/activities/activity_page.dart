5d6bbbfb9d58a8b4ff36c6fb99524b0ed9666978
graceyong <grace.yong.xue.min@gmail.com>
2022-07-15 04:37:16 +0800

    update

--- lib/pages/event_tab/activities/activity_page.dart
+++ lib/pages/event_tab/activities/activity_page.dart
@@ -1,22 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:gaigai_planner/models/activity.dart';
+import 'package:gaigai_planner/services/activity_service.dart';
 
 class ActivityPage extends StatefulWidget {
-  const ActivityPage({super.key, required this.name});
-  final String name;
+  const ActivityPage({super.key, required this.eventID});
+  final String eventID;
 
   @override
   State<ActivityPage> createState() => _ActivityPageState();
 }
 
 class _ActivityPageState extends State<ActivityPage> {
+  ActivityService activityClient = ActivityService();
+  List<Activity> list = [];
+  String dropDownValue = 'Default';
+
+  @override
+  void initState() {
+    super.initState();
+    getDefaultActivity();
+  }
+
+  getDefaultActivity() async {
+    list = await activityClient.getDefaultActivity();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.name),
-        centerTitle: true,
-      ),
+      body: Column(children: [
+        Row(
+          children: [
+            const Text('Sort by'),
+            DropdownButton(
+              value: dropDownValue,
+              icon: const Icon(Icons.arrow_drop_down_rounded),
+              items: const [
+                DropdownMenuItem(
+                  value: 'Default',
+                  child: Text('Default'),
+                ),
+                DropdownMenuItem(
+                  value: 'Cost',
+                  child: Text('Cost'),
+                ),
+                DropdownMenuItem(
+                  value: 'Distance',
+                  child: Text('Distance'),
+                ),
+              ],
+              onChanged: (String? value) async {
+                setState(() {
+                  dropDownValue = value!;
+                });
+                if (dropDownValue == 'Default') {
+                  getDefaultActivity();
+                  setState(() {});
+                } else if (dropDownValue == 'Cost') {
+                  setState(() {
+                    //sortByCost();
+                  });
+                } else if (dropDownValue == 'Distance') {
+                  setState(() {
+                    //sortByDistance();
+                  });
+                }
+              },
+            )
+          ],
+        )
+      ]),
     );
   }
 }
